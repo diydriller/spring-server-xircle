8c2bf6f2732ba37490821712716fb1a23f040ea5
diydriller <dhrhd080@naver.com>
2024-09-19 22:14:19 +0900

    feat: redis 설정

--- domain-core/build.gradle.kts
+++ domain-core/build.gradle.kts
@@ -12,4 +12,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     runtimeOnly("com.mysql:mysql-connector-j")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis:3.3.3")
 }
