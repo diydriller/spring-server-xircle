387795f71dd9746049cbd6e45db41f479a1b482c
diydriller <dhrhd080@naver.com>
2024-10-04 02:28:40 +0900

    fix: api 서버 - service discovery 서버 연결 설정 & api 서버 라우팅

--- api-server/build.gradle.kts
+++ api-server/build.gradle.kts
@@ -6,6 +6,10 @@ tasks.getByName("jar") {
     enabled = false
 }
 
+ext {
+    set("springCloudVersion", "2023.0.3")
+}
+
 dependencies {
     implementation(project(":domain-core"))
     implementation(project(":common"))
@@ -13,4 +17,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation:3.3.3")
     implementation("com.auth0:java-jwt:4.4.0")
     testImplementation("org.springframework.security:spring-security-test")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom ("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
