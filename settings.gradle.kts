ea8f280216ec12a11a6b1032910a22bb36e652ac
diydriller <dhrhd080@naver.com>
2024-12-07 12:17:01 +0900

    feat: 프로젝트 시작

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+rootProject.name = "xircle"
+include("common")
+include("gateway-service")
+include("discovery-service")
+include("user-service")
+include("post-service")
+include("follow-service")
