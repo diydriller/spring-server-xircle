0fda67ee27ffa454d2950fecbc949c68a56daba8
diydriller <dhrhd080@naver.com>
2024-12-07 12:17:45 +0900

    feat: gateway service 모듈 추가

---
+++ gateway-service/build.gradle.kts
@@ -0,0 +1,24 @@
+tasks.getByName("bootJar") {
+    enabled = true
+}
+
+tasks.getByName("jar") {
+    enabled = false
+}
+
+ext {
+    set("springCloudVersion", "2023.0.3")
+}
+
+dependencies {
+    implementation(project(":common"))
+    implementation ("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation ("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("com.auth0:java-jwt:4.4.0")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom ("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
