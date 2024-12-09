2c750f75a275ca9c739d8b2938458a23ad4f94d2
diydriller <dhrhd080@naver.com>
2024-12-09 12:08:22 +0900

    feat: config service 모듈 추가

---
+++ config-service/build.gradle.kts
@@ -0,0 +1,22 @@
+tasks.getByName("bootJar") {
+    enabled = true
+}
+
+tasks.getByName("jar") {
+    enabled = false
+}
+
+ext {
+    set("springCloudVersion", "2023.0.3")
+}
+
+dependencies {
+    implementation("org.springframework.cloud:spring-cloud-config-server")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom ("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
