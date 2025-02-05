f4318d8ac5db1bfd76a2e2dea7f11eebbe72c6dd
diydriller <dhrhd080@naver.com>
2025-02-05 19:52:25 +0900

    fix: follow service의 kafka consumer에서 outbox event 수신 후 manual commit 적용

--- follow-service/src/main/kotlin/com/xircle/followservice/config/KafkaConfig.kt
+++ follow-service/src/main/kotlin/com/xircle/followservice/config/KafkaConfig.kt
@@ -1,58 +1,26 @@
 package com.xircle.followservice.config
 
-import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.clients.producer.ProducerConfig
-import org.apache.kafka.common.IsolationLevel
-import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.kafka.common.serialization.StringSerializer
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
-import org.springframework.kafka.core.*
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
 import org.springframework.kafka.listener.ContainerProperties
-import org.springframework.kafka.support.serializer.JsonDeserializer
-import org.springframework.kafka.support.serializer.JsonSerializer
 
 @Configuration
 @EnableKafka
 class KafkaConfig {
-    @Value("\${kafka.broker}")
-    private val kafkaBroker: String? = null
-
     @Bean
-    fun kafkaTemplate(): KafkaTemplate<String, Any> {
-        return KafkaTemplate(producerFactory())
+    fun kafkaTemplate(producerFactory: ProducerFactory<String, Any>): KafkaTemplate<String, Any> {
+        return KafkaTemplate(producerFactory)
     }
 
     @Bean
-    fun producerFactory(): ProducerFactory<String, Any> {
-        val config: MutableMap<String, Any?> = HashMap()
-        config[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = kafkaBroker
-        config[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
-        config[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonSerializer::class.java
-        return DefaultKafkaProducerFactory(config)
-    }
-
-    @Bean
-    fun consumerFactory(): ConsumerFactory<String, Any> {
-        val config: MutableMap<String, Any?> = HashMap()
-        config[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = kafkaBroker
-        config[ConsumerConfig.GROUP_ID_CONFIG] = "user-group"
-        config[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
-        config[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java
-        config[JsonDeserializer.TRUSTED_PACKAGES] = "*"
-        config[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = false
-        config[ConsumerConfig.ISOLATION_LEVEL_CONFIG] = IsolationLevel.READ_COMMITTED
-        return DefaultKafkaConsumerFactory(config)
-    }
-
-    @Bean
-    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, Any> {
-        val factory =
-            ConcurrentKafkaListenerContainerFactory<String, Any>()
-        factory.consumerFactory = consumerFactory()
+    fun kafkaListenerContainerFactory(consumerFactory: ConsumerFactory<String, Any>): ConcurrentKafkaListenerContainerFactory<String, Any> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, Any>()
+        factory.consumerFactory = consumerFactory
         factory.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL
         return factory
     }
